7a61fa4669aba17b008bd865ad18ef0c96985052
Munna <104292892+Sardar-Murad-Ali@users.noreply.github.com>
2023-08-12 14:56:18 +0500

    Delete src directory

--- src/services/auth.service.js
+++
@@ -1,8 +0,0 @@
-import { api } from "./axios.service";
-
-export async function login(body){
-    return api.post("/login",body);
-}
-export async function register(body){
-    return api.post("/register",body);
-}
--- src/services/user.service.js
+++
@@ -1,8 +0,0 @@
-import { api } from "./axios.service";
-export async function getUserProfile(){
-    return await api.get("/user/profile");
-}
-
-export async function updateProfile(formData){
-    return await api.post("/user/update",formData);
-}
